0b1abe18b1604beb877b80351fafc658a29f57eb
FreitasPedro0 <pedrofreitassantos2021@gmail.com>
2023-09-17 16:11:55 -0300

    ex create

---
+++ aulas/aula12ex/ex015/script.js
@@ -0,0 +1,15 @@
+function verificar(){
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = window.document.getElementById('txtano')
+    var res = window.document.getElementById('res')
+    
+    if (fano.value.leght ==0 || Number(fano.value) > ano){
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else{
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+    }
+    
+    
+}
--- aulas/aula12ex/modelo/script.js
+++ aulas/aula12ex/modelo/script.js
@@ -1,1 +1,21 @@
-window.alert('Olá')
+function carregar(){
+var txt = window.document.getElementById('txt')
+var img = window.document.getElementById('img')
+var agora = new Date ()
+var hora = agora.getHours()
+txt.innerHTML = `Agora são ${hora} horas.`
+
+if (hora >= 0 && hora < 12){
+    // BOM DIA!
+    img.src = 'manhã.jpg'
+    document.body.style.background = '#e2cd9f'
+} else if (hora >= 12 && hora <= 18){
+    // BOA TARDE!
+    img.src = 'tarde.jpg'
+    document.body.style.background = '#b9846f'
+} else{
+    // BOA NOITE!
+    img.src = "noite.jpg"
+    document.body.style.background = '#515154'
+}
+}
