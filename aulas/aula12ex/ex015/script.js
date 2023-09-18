0207066854fc99e977be90dd54875c6239febb44
FreitasPedro0 <pedrofreitassantos2021@gmail.com>
2023-09-18 18:44:14 -0300

    estruturas condicionais fianlizadas

--- aulas/aula12ex/ex015/script.js
+++ aulas/aula12ex/ex015/script.js
@@ -9,7 +9,32 @@ function verificar(){
     } else{
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = window.document.getElementById('img')
+    } if (fsex[0].checked) {
+        genero = 'homem'
+        if(idade >=0 && idade < 10){
+            img.src = 'homem-b.jpg'
+        } else if (idade < 21){
+            img.src = 'homem-j.jpg'
+        } else if (idade < 50){
+            img.src = 'homem-a.jpg'
+        } else{
+            img.src = 'homem-i.jpg'
+        }
+    } else if (fsex[1].checked){
+        genero = 'mulher'
+        if(idade >=0 && idade < 10){
+            img.src = 'mulher-b.jpg'
+        } else if (idade < 21){
+            img.src = 'mulher-j.jpg'
+        } else if (idade < 50){
+            img.src = 'mulher-a.jpg'
+        } else{
+            img.src = 'mulher-i.jpg'
+        }
     }
     
-    
+    res.style.textAlign = 'center'
+    res.innerHTML = `Detectamos ${genero} com ${idade} anos`
 }
