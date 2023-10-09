293629b716e72cdf0899ab24030ec98f070e2075
PedroF7 <pedrofreitassantos2021@gmail.com>
2023-10-08 21:41:41 -0300

    finish

---
+++ aulas/aula13/node_4effbb2ce6a6c.tmp.js
@@ -0,0 +1,18 @@
+var c = 1
+
+do {
+    console.log(`passo ${c}`)
+} while (c <=6)
+
+// while (c <= 0){
+    //     console.log('Tudo bem?')
+    //     c++ //c = c + 1
+    // }
+
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
---
+++ aulas/aula13/node_d9b0ec6646457.tmp.js
@@ -0,0 +1,18 @@
+var c = 1
+
+while (c <= 2000000000){
+    console.log('Tudo bem?')
+    c++ //c = c + 1
+}
+
+
+
+
+
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
