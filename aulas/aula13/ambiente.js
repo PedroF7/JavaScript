6f085bab675a45306ab099f5ce257fd44a202067
PedroF7 <pedrofreitassantos2021@gmail.com>
2023-12-03 21:14:25 -0300

    ambiente2 create

--- aulas/aula13/ambiente.js
+++ aulas/aula13/ambiente.js
@@ -2,7 +2,7 @@ var c = 1
 
 do {
     console.log(`passo ${c}`)
-} while (c <=6)
+} while (c<=5)
 
 // while (c <= 0){
     //     console.log('Tudo bem?')
---
+++ aulas/aula13/ambiente2.js
@@ -0,0 +1,23 @@
+console.log("vai começar")
+for (c = 1;c <= 4;c){
+    console.log(c)
+}
+
+console.log("fim!")
+
+
+/*
+var c = 1
+while (c <= 5){
+    console.log(c)
+    c++
+} 
+*/
+
+/*
+var c = 1
+do(c <= 5)
+    console.log(c)
+    c++
+} while (c <= 5)
+*/ 
